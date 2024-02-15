97a699c1929998362e03f1ff18178c0869c75310
Ari-Rosalie <collado.arr@gmail.com>
2024-02-14 21:17:19 -0500

    All functions pass

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 /**
  * Returns an array containing only the words from the input array that start with a capital letter.
+ * 
+ * 
  * @param {string[]} stringsArray - An array of strings.
  * @returns {string[]} - An array of strings containing words starting with a capital letter.
  * @example
  * wordsStartingWithCapital(["Apple", "banana", "Cat", "dog"])
  * // Returns: ["Apple", "Cat"]
  */
-function wordsStartingWithCapital() {}
+function wordsStartingWithCapital(arr) {
+  // let answer = [];
+  // for(let word of arr){
+  //   //need to compare if the first letter of each word is Uppercase
+  //   if( word[0] === arr.toUpperCase) {
+  //     answer.push(word.toUpperCase)
+  //   }
+  // }
+  // return answer;
+
+  let answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i]
+    if(word[0] === word[0].toUpperCase()) {
+      answer.push(word)
+    }
+  }
+  return answer
+}
 
 /**
  * Calculates the sum of all odd numbers greater than 10 in the given array.
@@ -16,7 +36,15 @@ function wordsStartingWithCapital() {}
  * sumOfOddNumbersGreaterThanTen([5, 15, 10, 13, 20])
  * // Returns: 28
  */
-function sumOfOddNumbersGreaterThanTen() {}
+function sumOfOddNumbersGreaterThanTen(arr) {
+  let sum = 0;
+  for( let num of arr) {
+    if (num > 10 && num % 2 !== 0) {
+      sum += num
+    }
+  }
+  return sum
+}
 
 /**
  * Returns an object containing a count of how many letters are in each word in the given sentence.
@@ -26,7 +54,18 @@ function sumOfOddNumbersGreaterThanTen() {}
  * countLettersInEachWord("The quick brown fox")
  * // Returns: { "The": 3, "quick": 5, "brown": 5, "fox": 3 }
  */
-function countLettersInEachWord() {}
+
+function countLettersInEachWord(str) {
+  if (!str){
+    return {}
+  }
+  let answer = {}
+  let words = str.split(' ')
+  for(let word of words){
+    answer[word] = word.length
+  }
+  return answer
+}
 
 module.exports = {
   wordsStartingWithCapital,
