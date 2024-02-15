221a8164eff0e34b053e7c96ec84df020b3922c2
lynnjaco <jacobs.lynnette@gmail.com>
2024-02-14 20:20:03 -0500

    All Functions Tested & Completed

--- index.js
+++ index.js
@@ -6,8 +6,11 @@
  * wordsStartingWithCapital(["Apple", "banana", "Cat", "dog"])
  * // Returns: ["Apple", "Cat"]
  */
-function wordsStartingWithCapital() {}
+function wordsStartingWithCapital(array) {
+  return array.filter(ele => ele[0] === ele[0].toUpperCase());
+}
 
+console.log(wordsStartingWithCapital(["Apple", "banana", "Cat", "dog"]));
 /**
  * Calculates the sum of all odd numbers greater than 10 in the given array.
  * @param {number[]} numbersArray - An array of numbers.
@@ -16,7 +19,11 @@ function wordsStartingWithCapital() {}
  * sumOfOddNumbersGreaterThanTen([5, 15, 10, 13, 20])
  * // Returns: 28
  */
-function sumOfOddNumbersGreaterThanTen() {}
+function sumOfOddNumbersGreaterThanTen(array) {
+  return array.reduce((sum, num) => num % 2 && num > 10 ? sum + num : sum, 0)
+}
+
+console.log(sumOfOddNumbersGreaterThanTen([5, 15, 10, 13, 20]));
 
 /**
  * Returns an object containing a count of how many letters are in each word in the given sentence.
@@ -26,7 +33,13 @@ function sumOfOddNumbersGreaterThanTen() {}
  * countLettersInEachWord("The quick brown fox")
  * // Returns: { "The": 3, "quick": 5, "brown": 5, "fox": 3 }
  */
-function countLettersInEachWord() {}
+function countLettersInEachWord(string) {
+  let obj = {};
+  string.split(" ").reduce((acc, ele) => obj[ele] = ele.length, {})
+  return obj;
+}
+
+console.log(countLettersInEachWord(("The quick brown fox")));
 
 module.exports = {
   wordsStartingWithCapital,
