9c4b9049501d0b6362e24bff74b1d525527ccd71
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-02-14 20:23:23 -0500

    Function, countLettersInEachWord, completed using split method and for of loop

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
 const wordsStartingWithCapital = (stringsArray) => stringsArray.filter((elem) => elem[0] === elem[0].toUpperCase());
 
 
-console.log(wordsStartingWithCapital(["Apple", "banana", "Cat", "dog"]));
 
 /**
  * Calculates the sum of all odd numbers greater than 10 in the given array.
@@ -21,7 +20,6 @@ console.log(wordsStartingWithCapital(["Apple", "banana", "Cat", "dog"]));
  */
 const sumOfOddNumbersGreaterThanTen = (numbersArray) => numbersArray.filter((nums) => (nums > 10) && (nums & 2 !== 0)).reduce((a, b) => a + b, 0);
 
-console.log(sumOfOddNumbersGreaterThanTen([5, 15, 10, 13, 20]));
 
 /**
  * Returns an object containing a count of how many letters are in each word in the given sentence.
@@ -31,7 +29,18 @@ console.log(sumOfOddNumbersGreaterThanTen([5, 15, 10, 13, 20]));
  * countLettersInEachWord("The quick brown fox")
  * // Returns: { "The": 3, "quick": 5, "brown": 5, "fox": 3 }
  */
-function countLettersInEachWord(sentence) {}
+function countLettersInEachWord(sentence) {
+  if (!sentence) return {};
+
+  const bin = sentence.split(' ');
+  const result = {};
+
+  for (let word of bin) {
+    result[word] = word.length;
+  }
+
+  return result;
+}
 
 module.exports = {
   wordsStartingWithCapital,
